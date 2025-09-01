ef0e3fcc540665aff356fb83eb64d46a55c6ccfe
yurt35 <yurt35@gmail.com>
2025-09-01 14:33:28 +0200

    first

--- userdata.sh
+++ userdata.sh
@@ -7,12 +7,13 @@ apt install python3-pip -y
 pip3 install boto3
 apt  install awscli -y
 cd /home/ubuntu/
-TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /Morgoliath/capstone/token --with-decryption --query 'Parameter.Value' --output text)
-git clone https://$TOKEN@github.com/Morgoliath/aws-capstone.git
-cd /home/ubuntu/aws-capstone-project
-apt-get install python3.10-dev default-libmysqlclient-dev -y 
+TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /hasan/capstone/token --with-decryption --query 'Parameter.Value' --output text)
+# (private repo) --> git clone https://$TOKEN@github.com/Morgoliath/aws-capstone.git
+git clone https://github.com/hasanyurt35/capstone.git
+cd /home/ubuntu/capstone
+apt-get install python3.10-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/aws-capstone-project/src
+cd /home/ubuntu/capstone/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
