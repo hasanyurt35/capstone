f065f137000bba7af3979ec7bc2dad0f42a58234
yurt35 <yurt35@gmail.com>
2025-09-01 17:31:04 +0200

    second

--- userdata.sh
+++ userdata.sh
@@ -2,10 +2,13 @@
 apt-get update -y
 apt-get upgrade -y
 apt-get install git -y
+apt install unzip
 apt-get install python3 -y
 apt install python3-pip -y
 pip3 install boto3
-apt  install awscli -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 cd /home/ubuntu/
 TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /hasan/capstone/token --with-decryption --query 'Parameter.Value' --output text)
 # (private repo) --> git clone https://$TOKEN@github.com/Morgoliath/aws-capstone.git
